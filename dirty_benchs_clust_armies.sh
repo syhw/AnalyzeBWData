2bb2fa5c0495b175cab821cdcb14ffa7d2227a14
syhw <gabriel.synnaeve@gmail.com>
2012-05-21 18:56:23 +0200

    bench script for clustering/armies.py

---
+++ dirty_benchs_clust_armies.sh
@@ -0,0 +1,7 @@
+python clusterize/armies.py -d gosu_data/PvP/ -t > PvP.out
+python clusterize/armies.py -d gosu_data/PvT/ -t > PvT.out
+python clusterize/armies.py -d gosu_data/PvZ/ -t > PvZ.out
+python clusterize/armies.py -d gosu_data/TvT/ -t > TvT.out
+python clusterize/armies.py -d gosu_data/TvZ/ -t > TvZ.out
+python clusterize/armies.py -d gosu_data/ZvZ/ -t > ZvZ.out
+
